0e9db3b4a2cbafc25881310f1fdcb85655c4c151
JuliaPBV <vital.pereira.julia@gmail.com>
2024-04-08 11:17:18 -0300

    imprimir na tela a pergunta de quantos alunos você quer cadastrar, qual o nome do aluno e a nota, depois vai imprimir o nome e a nota do melhor aluno que foi cadastrado.

--- src/index.ts
+++ src/index.ts
@@ -40,4 +40,60 @@ app.listen(PORT, () => {
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
 	
+const readline = require ('readline');
+const rl = readline.createInterface({
+
+  input: process.stdin,
+  output: process.stdout
+});
+
+let quantidadeAlunos: number;
+let nomeMelhorAluno: string;
+let melhorNota: number = -Infinity; 
+
+function perguntarQuantidadeAlunos() {
+  rl.question('Quantos alunos você deseja cadastrar? ', (alunosQt: any) => {
+    quantidadeAlunos = parseInt(alunosQt); 
+    if (quantidadeAlunos <= 0 || isNaN(quantidadeAlunos)) {
+		console.log('Por favor, insira um número válido de alunos.');
+		perguntarQuantidadeAlunos(); 
+	} else {
+      cadastrarAlunos();
+    }
+  });
+}
+
+function cadastrarAlunos() {
+  let alunosCadastrados = 0;
+
+  function cadastrarProximoAluno() {
+    rl.question(`Nome do aluno ${alunosCadastrados + 1}: `, (nome: any) => {
+      rl.question(`Nota do aluno ${alunosCadastrados + 1}: `, (nota: any) => {
+        const notaAluno = parseFloat(nota); 
+        if (isNaN(notaAluno) || notaAluno < 0 || notaAluno > 10) {
+          console.log('Por favor, insira uma nota válida entre 0 e 10.');
+          cadastrarProximoAluno(); 
+        } else {
+          if (notaAluno > melhorNota) {
+            melhorNota = notaAluno;
+            nomeMelhorAluno = nome;
+          }
+
+          alunosCadastrados++;
+          if (alunosCadastrados < quantidadeAlunos) {
+            cadastrarProximoAluno();
+          } else {
+            console.log(`O aluno com a maior nota é ${nomeMelhorAluno} com nota ${melhorNota}`);
+            rl.close(); 
+          }
+        }
+      });
+    });
+  }
+
+  cadastrarProximoAluno();
+}
+
+perguntarQuantidadeAlunos();
+
 });
